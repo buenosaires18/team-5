eba47dfaebbbbb061166947fa27f833da6dd6f10
Laura <lauraagustinatrotta@gmail.com>
2018-05-26 01:28:09 -0300

    Subo la carpeta de springboot

---
+++ src/main/java/com/ensat/entities/Oferta.java
@@ -0,0 +1,54 @@
+package com.ensat.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ofertas")
+public class Oferta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable=false)
+	private int ID;
+	@Column(nullable=false)
+	private String titulo;
+	@Column(nullable=false)
+	private String descripcion;
+	
+	public Oferta(String titulo, String descripcion) {
+		super();
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+
+}
